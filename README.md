88c19108cc33ae20f64142f213258b5a17fb77a8
Sandy Maguire <sandy@sandymaguire.me>
2015-01-20 23:00:41 -0500

    SNAKE MOMENTUM

---
+++ README.md
@@ -0,0 +1,9 @@
+## Ferocious Snakes
+
+### Dedication
+
+> This is the stupidest, like, most, just, difficult to even care about game
+> I've ever seen in my life.
+>
+> --Arin Hanson, "Wild Snake - Game Grump VS"
+
--- Snakes.hs
+++ Snakes.hs
@@ -6,28 +6,44 @@ import qualified FRP.Helm.Time as Time
 import qualified FRP.Helm.Keyboard as Keyboard
 import qualified FRP.Helm.Window as Window
 
-data SnakeState = SnakeState {body :: [(Int, Int)]}
+data SnakeState = SnakeState {body :: [(Int, Int)], momentum :: (Int, Int)}
 
 snakeSignal :: Signal SnakeState
-snakeSignal = controlSignal
+snakeSignal = moveSignal
   where
-    controlSignal = foldp newState initialState (orthogonal <~ Keyboard.arrows)
-    orthogonal (x, y) | x == 0 = (x, y)
-                      | otherwise = (x, 0)
-    initialState = SnakeState { body = [(0,0), (0,1), (0,2), (1,2)] }
-    newState :: (Int, Int) -> SnakeState -> SnakeState
-    newState arrows@(dx, dy) state = state { body = newBody }
+    orthogonal (x, y) dt | x == 0    = ((0, y), dt)
+                         | otherwise = ((x, 0), dt)
+
+    initialState = SnakeState { body = [(0,0), (0,1), (0,2), (1,2)], momentum = (0, 1) }
+    newState :: ((Int, Int), Double) -> SnakeState -> SnakeState
+    newState ((dx, dy), dt) state = state { body = body', momentum = momentum' }
       where
-        newBody | arrows == (0,0) = body state
-                | otherwise = (tail $ body state) ++ [newPos]
-        newPos = (x + dx, y + dy)
-        (x, y) = last $ body state
+        momentum' | (dx, dy) == (0, 0) = momentum state
+                  | otherwise          = (dx, dy)
+        body' = (tail . body $ state) ++ [next]
+        next = (x + mx, y + my)
+        (mx, my) = momentum state
+        (x, y) = last . body $ state
+
+    moveSignal = foldp newState initialState
+        (orthogonal <~ Keyboard.arrows ~~ stepSignal)
+
+stepSignal :: Signal Double
+stepSignal = signal
+  where
+    signal = foldp step 0 (Time.fps 5)
+    step :: Time -> Double -> Double
+    step dt n = n + Time.inSeconds dt
+
+
+
 
 stroke = outlined $ solid red
 frame = stroke $ rect 400 760
 
 block :: Int -> Int -> Form
-block x y = move (fromIntegral $ 40 * x, fromIntegral $ 40 * y) $ filled white $ rect 40 40
+block x y = move (fromIntegral $ 40 * x, fromIntegral $ 40 * y) $
+    filled white $ rect 40 40
 
 snake :: SnakeState -> [Form]
 snake state = map (\(x, y) -> block x y) $ body state
